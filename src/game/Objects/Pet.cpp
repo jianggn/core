3bf89fe8d70fc9f8a9daa415de5cc6a449f63fff
jianggn <34902969+jianggn@users.noreply.github.com>
2025-02-05 12:11:23 +0800

    20250205 猎人天赋：兽王，宠物快乐值下降减缓

--- src/game/Objects/Pet.cpp
+++ src/game/Objects/Pet.cpp
@@ -795,6 +795,14 @@ void Pet::LooseHappiness()
             return;
         ModifyPower(POWER_HAPPINESS, addvalue);
     }
+    else if (pOwner->HasAura(34316))
+    {
+        ModifyPower(POWER_HAPPINESS, dither(-addvalue * 0.5f));
+    }
+    else if (pOwner->HasAura(34317))
+    {
+        ModifyPower(POWER_HAPPINESS, dither(-addvalue * 0.25f));
+    }
     else
     {
         ModifyPower(POWER_HAPPINESS, -addvalue);
