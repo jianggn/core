76ec76942e4e470b6c76392187433f759455b6ed
jianggn <804053980@qq.com>
2025-07-09 01:46:44 +0800

    20250709 术士-地狱猎犬 腐化之血仇恨值

--- src/game/Threat/ThreatManager.cpp
+++ src/game/Threat/ThreatManager.cpp
@@ -561,6 +561,14 @@ void ThreatManager::addThreat(Unit* pVictim, float threat, bool crit, SpellSchoo
             }
         }
     }
+    // Felhunter - Tainted Blood : add 7.5% max mana threat
+    else if (pThreatSpell && (pThreatSpell->Id == 19479 ||
+                              pThreatSpell->Id == 19652 ||
+                              pThreatSpell->Id == 19653 ||
+                              pThreatSpell->Id == 19654))
+    {
+        threat += pVictim->GetMaxPower(POWER_MANA)*0.075;
+    }
     // Hunter's Pet - Growl : add 10% max health threat
     else if (pThreatSpell && (pThreatSpell->Id == 2649 ||
                               pThreatSpell->Id == 14916 ||
