fb4a611259f553f4eb9cf1f0a5408e249dcebe2a
jianggn <804053980@qq.com>
2025-07-10 18:48:31 +0800

    1

--- src/game/Spells/SpellAuras.cpp
+++ src/game/Spells/SpellAuras.cpp
@@ -8611,7 +8611,7 @@ void SpellAuraHolder::CalculateHeartBeat(Unit* caster, Unit* target)
         // Seduction - spell 6358
         else if (m_spellProto->Id == 6358)
         {
-            if (Player* pOwner = caster->GetOwner())
+            if (Player* pOwner = ::ToPlayer(caster->GetOwner()))
             {
                 if (pOwner->HasAura(18756))
                     return;
