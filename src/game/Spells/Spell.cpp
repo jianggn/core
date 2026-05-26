093134c43309a343e4687daf6f1820b1072bd248
jianggn <804053980@qq.com>
2026-05-26 15:38:26 +0800

    20260526 萨满-黑鸟-不再对星体禁锢目标施放秘法天球

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -5700,7 +5700,10 @@ SpellCastResult Spell::CheckCast(bool strict)
         if (m_spellInfo->Id == 2855)
             if (!(target->IsPlayer() || (target->IsPet() && target->GetOwnerGuid().IsPlayer())))
                 return SPELL_FAILED_BAD_TARGETS;
-
+        // Arcane Orb mod
+        if (m_spellInfo->Id == 34085)
+            if (target->HasAura(34086))
+                return SPELL_FAILED_BAD_TARGETS;
         if (m_spellInfo->IsSpellAppliesAura() && !m_spellInfo->IsAreaOfEffectSpell())
         {
             // A more powerful spell is already active
