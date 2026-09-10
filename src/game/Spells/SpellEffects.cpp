ede201d8ad23ad084cb74400eeb2dafe7d835d3c
jianggn <34902969+jianggn@users.noreply.github.com>
2026-09-10 22:32:39 +0800

    1

--- src/game/Spells/SpellEffects.cpp
+++ src/game/Spells/SpellEffects.cpp
@@ -532,11 +532,11 @@ void Spell::EffectSchoolDMG(SpellEffectIndex effect_idx)
                     if (m_casterUnit)
                         damage = damage + (m_casterUnit->GetMaxHealth() * 0.15f);
                 }
-                // HUNTER - talent - Improved Arcane Shot - rank 5 : Arcane Shot damage bonus 33% Range AP
+                // HUNTER - talent - Improved Arcane Shot - rank 5 : Arcane Shot damage bonus 25% Range AP
                 else if (m_spellInfo->IsFitToFamilyMask<CF_HUNTER_ARCANE_SHOT>())
                 {
                     if (m_casterUnit->HasAura(19458))
-                        damage = damage + (m_casterUnit->GetTotalAttackPowerValue(RANGED_ATTACK) * 0.33f);
+                        damage = damage + (m_casterUnit->GetTotalAttackPowerValue(RANGED_ATTACK) * 0.25f);
                 }
                 break;
             }
